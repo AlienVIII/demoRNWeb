0b827740fc4770b01b536a00f9edd8c3e297ec54
Min <alien_viii@outlook.com>
2021-04-27 14:11:40 +0700

    Update babel.config.js
    add resolver

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,33 @@
-module.exports = {
-  presets: ["module:metro-react-native-babel-preset"],
+module.exports = (api) => {
+  const babelEnv = api.env();
+  const plugins = [
+    [
+      "module-resolver",
+      {
+        root: ["./src"],
+        extensions: [
+          ".ios.ts",
+          ".android.ts",
+          ".ios.tsx",
+          ".android.tsx",
+          ".jsx",
+          ".js",
+          ".json",
+          ".tsx",
+          ".ts",
+        ],
+        alias: {
+          test: "./test",
+        },
+      },
+    ],
+  ];
+  if (babelEnv !== "development") {
+    // 'transform-remove-console' production env eslint version <6
+    // plugins.push(['transform-remove-console', {exclude: ['error', 'warn']}]);
+  }
+  return {
+    presets: ["module:metro-react-native-babel-preset"],
+    plugins,
+  };
 };
